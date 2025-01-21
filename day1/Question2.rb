d9a2e6bd7ad6c64263dde7c47655b47f50d37c4a
mehulsingh010 <mehul.singh@joshsoftware.com>
2025-01-21 14:52:39 +0530

    refactor: Replace if-else with switch-case and Add proper indentation

--- day1/Question2.rb
+++ day1/Question2.rb
@@ -9,22 +9,24 @@ if grade is between otherwise return college
 
 =end
 
-puts "enetr the grade "
+puts "Enter the grade "
 grade = gets.to_i
 
 def determine_school_level(grade)
-  if grade >=1 && grade <= 5
-      return "Elementary"
-  elsif grade >= 6 && grade <= 8
-      return "middle School"
-  elsif grade >= 9 && grade <=12
+  case grade
+  
+  when 1..5
+    return "Elementary"
+  when 6..8
+    return "middle School"
+  when 9..12
     return "high school"
-  else 
-      return "college"
+  else
+    return "college"
   end
 end
 
-puts "School Level Is : " , determine_school_level(grade)
+puts "School Level Is : #{determine_school_level(grade)}" 
 
 
 
