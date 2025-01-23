8f8af6572babdc9e81ec291a24e632624d884ab7
mehulsingh010 <mehul.singh@joshsoftware.com>
2025-01-23 23:24:26 +0530

    refactor : breakdown the code into different meaningfull function

--- day3/mehul_hangman_using_class_v1.rb
+++ day3/mehul_hangman_using_class_v1.rb
@@ -1,13 +1,18 @@
-# frozen_string_literal: true
-
 class HangmanGame
   def initialize
     @curr_level = 1
+    @guessed_chars = []
+    @chances = 5
+  end
+
+  def welcome_message
     puts '-------------  Welcome to Hangman Game! ------------'
     puts ''
     puts "------------- LET'S PLAY! ------------"
     puts ''
-    
+  end
+
+  def user_input
     puts ''
     puts 'Do You Want to Play?'
     print "Enter 'yes' to play or 'no' to exit: "
@@ -24,93 +29,114 @@ class HangmanGame
   def game_level
     case @curr_level
     when 1
-      words = %w[usa india japan china italy spain]
-      hint = 'The category is: Country .'
+      @words = %w[usa india japan china italy spain]
+      @hint = 'The category is: Country.'
     when 2
-      words = %w[apple banana cherry grape mango orange]
-      hint = 'The category is: Fruits.'
+      @words = %w[apple banana cherry grape mango orange]
+      @hint = 'The category is: Fruits.'
     when 3
-      words = %w[elephant giraffe dolphin penguin zebra kangaroo]
-      hint = 'The category is: Animals.'
+      @words = %w[elephant giraffe dolphin penguin zebra kangaroo]
+      @hint = 'The category is: Animals.'
     when 4
-      words = %w[tokyo paris berlin madrid ottawa canberra]
-      hint = 'The category is: Capital cities.'
+      @words = %w[tokyo paris berlin madrid ottawa canberra]
+      @hint = 'The category is: Capital cities.'
     when 5
-      words = %w[galaxy nebula asteroid comet quasar satellite]
-      hint = 'The category is: Space terms.'
+      @words = %w[galaxy nebula asteroid comet quasar satellite]
+      @hint = 'The category is: Space terms.'
     else
-      puts 'Wrong Entry of Level Code'
+      puts 'Invalid Level.'
       return
     end
 
-    puts ""
-    puts "HINT ----->>  #{hint} "
-    puts ""
-    @word = words.sample
-    @guess_word = Array.new(@word.length, '_')
-    @guessed_chars = [] # Initialize an empty array to track guessed characters
-    puts "The word has #{@word.length} letters."
-    puts ' ', @guess_word.join(' ')
-    main_game
+    start_game
   end
 
-  def main_game
-    changes = 5
-    while changes > 0
-      puts ''
-      puts "Number Of Chances You Have Left: #{changes}"
-      puts "Characters you have guessed so far: #{@guessed_chars.join(', ')}" unless @guessed_chars.empty?
-      print 'Enter the character you would like to guess: '
-      char = gets.chomp.downcase
+  def start_game
+    puts ''
+    puts "HINT ----->>  #{@hint}"
+    puts ''
+    @word = @words.sample
+    @guess_word = Array.new(@word.length, '_')
+    @guessed_chars = []
+    @chances = 5
+    puts "The word has #{@word.length} letters."
+    puts '', @guess_word.join(' ')
+    play_game
+  end
 
-      if char.match(/^[a-z]$/)
-        if @guessed_chars.include?(char)
-          puts "You have already guessed the character '#{char}'. Try a different one."
-          next
+  def get_user_guess_char
+    puts ''
+    puts "Number of Chances You Have Left: #{@chances}"
+    puts "Characters you have guessed so far: #{@guessed_chars.join(', ')}" unless @guessed_chars.empty?
+    print 'Enter the character you would like to guess: '
+    gets.chomp.downcase
+  end
+
+  def process_guess(char)
+    if char.match(/^[a-z]$/)
+      if @guessed_chars.include?(char)
+        puts "You have already guessed the character '#{char}'. Try a different one."
+        return false
+      end
+
+      @guessed_chars << char
+
+      if @word.include?(char)
+        @word.chars.each_with_index do |w_char, index|
+          @guess_word[index] = char if w_char == char
         end
-
-        @guessed_chars << char # Add the character to the guessed list
-
-        if @word.include?(char)
-          @word.chars.each_with_index do |w_char, index|
-            @guess_word[index] = char if w_char == char
-          end
-          puts "Guessed character is right: #{char}"
-          puts @guess_word.join(' ')
-          puts ''
-        else
-          changes -= 1
-          puts 'Wrong guess! You lost one chance.'
-        end
-
-        if @guess_word.join('') == @word
-          puts "Congratulations! You guessed the word: #{@word}"
-          if @curr_level == 5
-            puts "Amazing! You've completed all levels of the Hangman Game. You're a champion!"
-            puts 'Thanks for playing! Goodbye!'
-            return
-          else
-            @curr_level += 1
-            puts ''
-            print 'Do you want to continue to the next level? (yes/no): '
-            continue = gets.chomp.downcase
-            if continue == 'yes'
-              game_level
-            else
-              puts 'Thanks for playing! Goodbye!'
-            end
-            return
-          end
-        end
+        puts "Good guess!"
       else
-        puts 'Invalid input! Please enter a single alphabetic character.'
+        @chances -= 1
+        puts 'Wrong guess! You lost one chance.'
+      end
+
+      puts @guess_word.join(' ')
+      return true
+    else
+      puts 'Invalid input! Please enter a single alphabetic character.'
+      return false
+    end
+  end
+
+  def handle_level_completion
+    if @guess_word.join('') == @word
+      puts "Congratulations! You guessed the word: #{@word}"
+      if @curr_level == 5
+        puts "Amazing! You've completed all levels of the Hangman Game. You're a champion!"
+        puts 'Thanks for playing! Goodbye!'
+        return true
+      else
+        @curr_level += 1
+        puts ''
+        print 'Do you want to continue to the next level? (yes/no): '
+        continue = gets.chomp.downcase
+        if continue == 'yes'
+          game_level
+        else
+          puts 'Thanks for playing! Goodbye!'
+        end
+        return true
       end
     end
+    false
+  end
 
-    puts "Game Over! The word was: #{@word}" if changes.zero?
+  def play_game
+    while @chances > 0
+      char = get_user_guess_char
+      next unless process_guess(char)
+      break if handle_level_completion
+    end
+
+    if @chances == 0 && !handle_level_completion
+      puts "Game Over! The word was: #{@word}"
+      exit
+    end
   end
 end
 
 # Start the game
-HangmanGame.new
-
+game = HangmanGame.new
+game.welcome_message
+game.user_input
